8f88441a705e32c9be29918e38360db273b63e5b
Rameez Ali <ra2252252@gmail.com>
2024-04-08 00:24:34 +0500

    ocument

--- main.ts
+++ main.ts
@@ -1,30 +1,31 @@
 #! usr/bin/env node
 
+import inquirer from "inquirer";
 
-import inquirer from 'inquirer'
-import chalk from 'chalk';
+import chalk from "chalk";
 
 console.log(chalk.bold.red(`\tWelcome To BMI Calculator`));
 
 let userAnswer = await inquirer.prompt([
-    {
-        name : "weightInKg",
-        message : "Enter Your Weight In Kg",
-        type : "number"
 
-    },
-    {
-        name : "heightInMeter",
-        message : "Enter Your Height In Meter",
-        type : "number"
+  {
+    name: "weightInKg",
+    message: "Enter Your Weight In Kg",
+    type: "number",
+  },
 
-    }
-])
+  {
+    name: "heightInMeter",
+    message: "Enter Your Height In Meter",
+    type: "number",
+  },
+  
+]);
 
-let userWeight = userAnswer.weightInKg
+let userWeight = userAnswer.weightInKg;
 
-let userHeight = userAnswer.heightInMeter
+let userHeight = userAnswer.heightInMeter;
 
-let bmi = userWeight / (userHeight * userHeight)
+let bmi = userWeight / (userHeight * userHeight);
 
 console.log(`Your BMI Is ${bmi}`);
